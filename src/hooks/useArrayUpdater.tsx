c147afd9860ab82cbbcddba523c754629afd4038
taishinaritomi <taishinaritomi@gmail.com>
2022-06-26 20:03:47 +0900

    update useObjectArrayUpdater

--- src/hooks/useArrayUpdater.tsx
+++ src/hooks/useArrayUpdater.tsx
@@ -1,27 +1,27 @@
-import type { Dispatch, SetStateAction} from 'react';
+import type { Dispatch} from 'react';
+import { useState} from 'react';
 import { arrayMove } from '~/libs/array';
 
-type Setter<T> = Dispatch<SetStateAction<T>>;
+type Setter<T> = Dispatch<((prevState: T) => T)>;
 
 const useObjectArrayUpdater = <
-  T extends Record<string | number | symbol, unknown>,
-  N = never,
-  K extends keyof T = never
+  T extends Record<string, unknown>,
+  K extends keyof T = 'id'
 >(
-  setter: Setter<T[] | N>,
-  key:K,
+  setter: Setter<T[]> | Setter<T[] | undefined> | Setter<T[] | null>,
+  key:K = 'id' as K,
 ) => {
 
   const add = (addValue: T, location: 'start' | 'end' = 'start') => {
     setter((array) => {
-      if(!Array.isArray(array)) return array;
+      if(!Array.isArray(array)) return [addValue];
       return location === 'start' ? [addValue, ...array] : [...array, addValue];
     });
   };
 
   const update = (updateKey: T[K], updateValue: Partial<T>) => {
     setter((array) => {
-      if(!Array.isArray(array)) return array;
+      if(!Array.isArray(array)) return [];
       return array.map((value) =>
         value[key] === updateKey ? { ...value, ...updateValue } : value,
       );
@@ -30,14 +30,13 @@ const useObjectArrayUpdater = <
 
   const remove = (removeKey: T[K]) => {
     setter((array) => {
-      if(!Array.isArray(array)) return array;
+      if(!Array.isArray(array)) return [];
       return array.filter((value) => value[key] !== removeKey);
     });
   };
-
   const move = (oldKey: T[K], newKey: T[K]) => {
     setter((array) => {
-      if(!Array.isArray(array)) return array;
+      if(!Array.isArray(array)) return [];
       const oldIndex = array.findIndex((value) => value[key] === oldKey);
       const newIndex = array.findIndex((value) => value[key] === newKey);
       return arrayMove(array, oldIndex, newIndex);
@@ -46,7 +45,7 @@ const useObjectArrayUpdater = <
 
   const nextMove = (nextMoveKey: T[K]) => {
     setter((array) => {
-      if(!Array.isArray(array)) return array;
+      if(!Array.isArray(array)) return [];
       const index = array.findIndex((value) => value[key] === nextMoveKey);
       return index === 0 ? array : arrayMove(array, index, index - 1);
     });
@@ -54,7 +53,7 @@ const useObjectArrayUpdater = <
 
   const prevMove = (prevMoveKey: T[K]) => {
     setter((array) => {
-      if(!Array.isArray(array)) return array;
+      if(!Array.isArray(array)) return [];
       const index = array.findIndex((value) => value[key] === prevMoveKey);
       return index === array.length - 1
         ? array
@@ -66,3 +65,23 @@ const useObjectArrayUpdater = <
 };
 
 export default useObjectArrayUpdater;
+
+const List = () => {
+  // Success Type
+  const [,setList] = useState<({name:string;id:number})[]>([]);
+  const { remove } = useObjectArrayUpdater(setList);
+
+  // const [,setList2] = useState<({id:string})[]>();
+  // const {remove:remove2} = useObjectArrayUpdater(setList2,"id");
+
+  // const [,setList4] = useState<({id:string})[] | undefined>(undefined);
+  // const {remove:remove4} = useObjectArrayUpdater(setList4,"id");
+
+  //   // Error Type
+  // const [,setList3] = useState<({id:string})[] | string>([]);
+  // const {remove:remove3} = useObjectArrayUpdater(setList3,"id");
+
+  return (
+    <></>
+  )
+}
