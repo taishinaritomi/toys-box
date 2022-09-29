2278a1bbf7d00f7f9ad8969a8418e9df0d65616b
taishinaritomi <taishinaritomi@gmail.com>
2022-09-29 16:15:24 +0900

    fix code format

--- cryptEmojiCLI.ts
+++ cryptEmojiCLI.ts
@@ -1,7 +1,7 @@
-import { decrypt } from './src/libs/crypto'
+import { decrypt } from './src/libs/crypto';
 
 const args = process.argv.slice(2);
 
 const text = args[0] || '';
 const pass = args[1] || '';
-console.log(decrypt(text,pass));
+console.log(decrypt(text, pass));
