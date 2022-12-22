ce5edf0bba846f4a46c2a3b6ba3725da48ca2630
taishinaritomi <taishinaritomi@gmail.com>
2022-12-22 20:45:47 +0900

    fix

--- src/app/todo-revase/view.tsx
+++ src/app/todo-revase/view.tsx
@@ -3,10 +3,14 @@
 export const TodoView = () => {
   return (
     <>
-      <div>tasks:{window?.localStorage.getItem('tasks')}</div>
-      <div>task:{window?.localStorage.getItem('task')}</div>
-      <div>todo:{window?.localStorage.getItem('todo')}</div>
-      <div>todos:{window?.localStorage.getItem('todos')}</div>
+    {typeof window !== 'undefined' && (
+      <>
+        <div>tasks:{window?.localStorage.getItem('tasks')}</div>
+        <div>task:{window?.localStorage.getItem('task')}</div>
+        <div>todo:{window?.localStorage.getItem('todo')}</div>
+        <div>todos:{window?.localStorage.getItem('todos')}</div>
+      </>
+    )}
     </>
   );
 };
